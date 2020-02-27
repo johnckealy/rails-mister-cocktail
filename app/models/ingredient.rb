f9ed802d7d76b8889cdd6782b3e0e7bf921dbfcd
John Kealy <johnckealy@gmail.com>
2020-02-27 16:21:39 +0800

    added delete and edit features

---
+++ app/models/ingredient.rb
@@ -0,0 +1,6 @@
+class Ingredient < ApplicationRecord
+  has_many :doses
+  has_one :cocktail, through: :doses, dependent: :restrict_with_exception
+
+  validates :name, presence: true, uniqueness: true
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+lemon = Ingredient.create(name: "lemon")
+ice = Ingredient.create(name: "ice")
+
+mojito = Cocktail.create(name: "Mojito")
+
+dose = Dose.new(description: "6 of")
+dose.ingredient = lemon
+dose.cocktail = mojito
+dose.save
+
+doseice3 = Dose.new(description: "3 cubes")
+doseice3.ingredient = ice
+dose.cocktail = mojito
+dose.save
+
+
+negroni = Cocktail.create(name: "Negroni")
+dosesplash = Dose.new(description: "A splash")
+dosesplash.ingredient = ice
+dosesplash.cocktail = negroni
+dosesplash.save
