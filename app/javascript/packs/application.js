392326aff741585246818300dfc56abe4273ca01
John Kealy <johnckealy@gmail.com>
2020-02-28 17:39:02 +0800

    patching configuration bug for bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -17,7 +17,7 @@
 
 console.log('Hello World from Webpacker')
 
-// import 'bootstrap';
+import 'bootstrap';
 
 import { loadDynamicBannerText } from '../components/banner';
 loadDynamicBannerText();
