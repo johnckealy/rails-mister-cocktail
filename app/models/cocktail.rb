16e689a7f2e90d94807ec075e3fa39da20315ff6
John Kealy <johnckealy@gmail.com>
2020-02-28 14:18:33 +0800

    completed image upload feature

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,6 +1,21 @@
 class Cocktail < ApplicationRecord
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
+  has_one_attached :photo
 
   validates :name, presence: true, uniqueness: true
+
+  def image_pth(options = {})
+      if self.photo.attached?
+        if options[:banner]
+          options = {width: 800, height: 300, crop: :fill}
+        else
+          options = {width: 500, height: 500, crop: :fill}
+        end
+
+        Cloudinary::Utils.cloudinary_url(self.photo.key, options)
+      else
+        ActionController::Base.helpers.asset_path('no_image.jpg')
+      end
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,31 +7,49 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 lemon = Ingredient.create(name: "lemon")
 ice = Ingredient.create(name: "Crushed Ice")
+tonic = Ingredient.create(name: "Tonic")
+gin = Ingredient.create(name: "Gin")
+rum = Ingredient.create(name: "Gin")
 Ingredient.create(name: "Rum")
 Ingredient.create(name: "Vodka")
 Ingredient.create(name: "Ginger Beer")
-Ingredient.create(name: "Tonic")
 Ingredient.create(name: "Tequila")
 Ingredient.create(name: "Ginger Ale")
 Ingredient.create(name: "Absenthe")
 Ingredient.create(name: "Ayauasca")
-Ingredient.create(name: "MDMA")
 
+# gin and tonic
 mojito = Cocktail.create(name: "Mojito")
+file = File.open('/home/jokea/Desktop/Cocktail_images/mojito.jpg')
+mojito.photo.attach(io: file, filename: 'mojito.jpg', content_type: 'image/jpg')
+mojito.save
+ice_dose = Dose.new(description: "5 cubes, crushed")
+ice_dose.ingredient = ice
+ice_dose.cocktail = mojito
+ice_dose.save
+rum_dose = Dose.new(description: "2 parts")
+rum_dose.ingredient = rum
+rum_dose.cocktail = mojito
+rum_dose.save
 
-dose = Dose.new(description: "6 of")
-dose.ingredient = lemon
-dose.cocktail = mojito
-dose.save
-
-doseice3 = Dose.new(description: "3 cubes")
-doseice3.ingredient = ice
-dose.cocktail = mojito
-dose.save
-
-
-negroni = Cocktail.create(name: "Negroni")
-dosesplash = Dose.new(description: "A splash")
-dosesplash.ingredient = ice
-dosesplash.cocktail = negroni
-dosesplash.save
+# gin and tonic
+gandt = Cocktail.create(name: "G & T")
+file = File.open('/home/jokea/Desktop/Cocktail_images/sexonbeach.jpg')
+gandt.photo.attach(io: file, filename: 'sexonbeach.jpg', content_type: 'image/jpg')
+gandt.save
+ice_dose = Dose.new(description: "3 cubes, crushed")
+ice_dose.ingredient = ice
+ice_dose.cocktail = gandt
+ice_dose.save
+lemon_dose = Dose.new(description: "A piece or two")
+lemon_dose.ingredient = lemon
+lemon_dose.cocktail = gandt
+lemon_dose.save
+gin_dose = Dose.new(description: "2 parts")
+gin_dose.ingredient = gin
+gin_dose.cocktail = gandt
+gin_dose.save
+tonic_dose = Dose.new(description: "6 parts")
+tonic_dose.ingredient = tonic
+tonic_dose.cocktail = gandt
+tonic_dose.save
